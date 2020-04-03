708ee52ccce69cc5f18c4964346faef0ed510fef
PauloAlves8039 <pj.alves547@gmail.com>
2020-04-03 18:50:31 -0300

    Adicionando novos elementos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
+/**
+ * Função responsável pelas configurações de exibição do modal.
+ * 
+ */
 function onOff(){
     document
         .querySelector("#modal")
         .classList
         .toggle("hide")
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")    
 }
 
